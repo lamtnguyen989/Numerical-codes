4d09ba093f64b063176ff3a8ed61150f15165bd0
lamtnguyen989 <lamtnguyen989@gmail.com>
2024-05-20 08:07:47 -0500

    Attemptted fix but issues prevail

--- Wave equation/waveFD.m
+++ Wave equation/waveFD.m
@@ -6,20 +6,20 @@
 %         number of space steps M,
 %         number of time steps N
 
-% INCOMPLETE!!!
+% BROKEN!!!
 function w = waveFD(x_start,x_end,t_start,t_end,M,N)
   % Initial info of the wave PDE
   f = @(x) x.^5;
   f_t = @(x) 10*x.^4;
   l = @(t) 32*t.^5;
-  r = @(t) (1+2*t)*t.^5;
+  r = @(t) (1+2*t.^1).^5;
   D = 2;
 
   % Setting up step sizes and alpha = D*k/h
-  m = M - 1;
+  h = (x_end - x_start)/M;
+  k = (t_end - t_start)/N;
+  m = M-1;
   n = N;
-  h = (x_end-x_start)/m;
-  k = (t_end-t_start)/n;
   alpha = (D*k)/h;
 
   % CFL condition check
@@ -32,20 +32,28 @@ function w = waveFD(x_start,x_end,t_start,t_end,M,N)
   A = diag(2-2*(alpha^2)*ones(m,1)) + diag((alpha^2)*ones(m-1,1),1) + diag((alpha^2)*ones(m-1,1),-1);
 
   % Initialize the solution
-  w(:,1) = transpose(f(x_start+(1:m)*h));
-  w(:,2) = (1/2)*A*w(:,1) + k*transpose(f_t(x_start + (1:m)*h)) + (1/2)*(alpha^2)*[l(t_start); zeros(m-2,1); r(t_start)];
+  w(:,1) = (1/2)*A*transpose(f(x_start + (1:m)*h)) + k*transpose(f_t(x_start + (1:m)*h));
+  w(:,2) = A*w(:,1) - transpose(f(x_start + (1:m)*h));
 
   % Iterations
   for j = 2:n
-    w(:,j+1) = A*w(:,j) - w(:,j-1) + (alpha^2)*[l(t_start + k*j); zeros(m-2,1); r(t_start + k*j)];
+    w(:,j+1) = A*w(:,j) - w(:,j-1);
   end
 
+  % Boundary attaching
+  w = [alpha^2*l((0:n)*k + t_start) ; w ; alpha^2*r((0:n)*k + t_start)];
+  w(1,1) = w(1,1)/2;
+  w(m+2, 1) = w(m+2,1)/2;
+
   % Plotting
-  x = (0:m-1)*h + ones(1,m)*x_start;
-  t = (0:n)*k + ones(1,n+1)*t_start;
+  x = (0:m+1)*h + x_start;
+  t = (0:n)*k + t_start;
   mesh(x,t,w')
+  xlabel('x');
+  ylabel('t');
   xlabel("x");
   ylabel("t");
   zlabel("u(x,t)")
   view(60,30);
+  axis([x_start x_end t_start t_end -1.25 1.25])
   axis([x_start, x_end, t_start, t_end, min(w(:))-0.25, max(w(:))+0.25])
