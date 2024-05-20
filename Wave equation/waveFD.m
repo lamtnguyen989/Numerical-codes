d01cabca87a9bb4d390019e025ed9dde1b480425
lamtnguyen989 <lamtnguyen989@gmail.com>
2024-05-20 04:35:44 -0500

    New issue discovered and added a new testing function

--- Wave equation/waveFD.m
+++ Wave equation/waveFD.m
@@ -1,22 +1,25 @@
 % Finite-difference for wave equation with specified initial conditions
 % (on retangular domain)
 %
-% @param: space interval [x_0, x_end],
-%         time interval [t_0, t_end],
-%         number of space steps m,
-%         number of time steps n
+% @param: space interval [x_start, x_end],
+%         time interval [t_start, t_end],
+%         number of space steps M,
+%         number of time steps N
 
-function w = waveFD(x_0,x_end,t_0,t_end,m,n)
+% INCOMPLETE!!!
+function w = waveFD(x_start,x_end,t_start,t_end,M,N)
   % Initial info of the wave PDE
-  f = @(x) 0*x;
-  l = @(t) 0*t;
-  r = @(t) 0*t;
-  f_t = @(x) 2*pi*sin(pi*x);
+  f = @(x) x.^5;
+  f_t = @(x) 10*x.^4;
+  l = @(t) 32*t.^5;
+  r = @(t) (1+2*t)*t.^5;
   D = 2;
 
   % Setting up step sizes and alpha = D*k/h
-  h = (x_end-x_0)/m;
-  k = (t_end-t_0)/n;
+  m = M - 1;
+  n = N;
+  h = (x_end-x_start)/m;
+  k = (t_end-t_start)/n;
   alpha = (D*k)/h;
 
   % CFL condition check
@@ -29,20 +32,20 @@ function w = waveFD(x_0,x_end,t_0,t_end,m,n)
   A = diag(2-2*(alpha^2)*ones(m,1)) + diag((alpha^2)*ones(m-1,1),1) + diag((alpha^2)*ones(m-1,1),-1);
 
   % Initialize the solution
-  w(:,1) = transpose(f(x_0+(1:m)*h));
-  w(:,2) = (1/2)*A*w(:,1) + k*transpose(f_t(x_0 + (1:m)*h)) + (1/2)*(alpha^2)*[l(t_0); zeros(m-2,1); r(t_0)];
+  w(:,1) = transpose(f(x_start+(1:m)*h));
+  w(:,2) = (1/2)*A*w(:,1) + k*transpose(f_t(x_start + (1:m)*h)) + (1/2)*(alpha^2)*[l(t_start); zeros(m-2,1); r(t_start)];
 
   % Iterations
   for j = 2:n
-    w(:,j+1) = A*w(:,j) - w(:,j-1) + (alpha^2)*[l(t_0 + k*j); zeros(m-2,1); r(t_0 + k*j)];
+    w(:,j+1) = A*w(:,j) - w(:,j-1) + (alpha^2)*[l(t_start + k*j); zeros(m-2,1); r(t_start + k*j)];
   end
 
   % Plotting
-  x = (0:m-1)*h + ones(1,m)*x_0;
-  t = (0:n)*k + ones(1,n+1)*t_0;
+  x = (0:m-1)*h + ones(1,m)*x_start;
+  t = (0:n)*k + ones(1,n+1)*t_start;
   mesh(x,t,w')
   xlabel("x");
   ylabel("t");
   zlabel("u(x,t)")
   view(60,30);
-  axis([x_0, x_end, t_0, t_end, min(w(:))-0.25, max(w(:))+0.25])
+  axis([x_start, x_end, t_start, t_end, min(w(:))-0.25, max(w(:))+0.25])
